7ad1fa8e49e4aae3a31998581d2ff970007980b2
ofonodomatt <amplifiedmatt@gmail.com>
2022-07-03 14:54:18 -0700

    Lowercase

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,10 +7,15 @@
  */
 int main(void)
 {
-	int num;
-
-	for (num = 0; num < 10; num++)
-		printf("%d", num);
-	printf("\n");
+	char ch = 'a'
+		while (ch <= 'z')
+		{
+			if (ch != 'e' && ch != 'q')
+			{
+				putchar(ch);
+			}
+			ch++;
+		}
+	putchar('\n');
 	return (0);
 }

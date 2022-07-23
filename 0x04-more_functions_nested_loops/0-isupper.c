ff3b41d214c7ab643a4c0358c3bcbe4dca1ef9b2
ofonodomatt <amplifiedmatt@gmail.com>
2022-07-22 19:51:48 -0700

    My first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - this is a function to check upper case character
+ * @c: an integer parameter
+ * Return: something but success
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
